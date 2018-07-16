058ef30d3699a423ef9f20d1af6aa2bd4c066b72
projectormato <k.keita811.anuy@gmail.com>
2018-07-17 00:57:03 +0900

    fix dir structure.

---
+++ src/Euler/p1.scala
@@ -0,0 +1,11 @@
+object p1 {
+  def main(args: Array[String]) :Unit = {
+    var ans = 0
+    for (i <- 0 until 1000) {
+      if (i % 3 == 0 || i % 5 == 0) {
+        ans += i
+      }
+    }
+    println(ans)
+  }
+}
