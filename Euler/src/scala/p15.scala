30b4cd9523807a2fdc0ffc200dbd269c1cb66e57
projectormato <k.keita811.anuy@gmail.com>
2018-10-25 12:07:24 +0900

    add Euler q15 code and fix q16 code.

--- Euler/src/scala/p15.scala
+++ Euler/src/scala/p15.scala
@@ -1,9 +1,7 @@
 object p15 extends App {
-  val mass = 20; var ans:Long = 1; var kaijou:Long = 1
+  val mass = 20; var ans:Long = 1;
   for (i <- 1 to mass) {
-    kaijou *= i
     ans = ans * (mass*2 - i + 1) / i
   }
   println(ans) // 137846528820
-  println(kaijou)
 }
