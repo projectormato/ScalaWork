9488f1f55c2d7ca48a966354f56bef759a6568e2
projectormato <k.keita811.anuy@gmail.com>
2019-02-06 23:40:44 +0900

    add Type parameter of dwango text.

---
+++ src/main/scala/dwango/Class/Stack.scala
@@ -0,0 +1,29 @@
+package dwango.Class
+
+trait Stack[+A] {
+  def push[E >: A](e: E): Stack[E]
+  def top: A
+  def pop: Stack[A]
+  def isEmpty: Boolean
+}
+
+class NonEmptyStack[+A](private val first: A, private val rest: Stack[A]) extends Stack[A] {
+  def push[E >: A](e: E): Stack[E] = new NonEmptyStack[E](e, this)
+  def top: A = first
+  def pop: Stack[A] = rest
+  def isEmpty: Boolean = false
+}
+
+case object EmptyStack extends Stack[Nothing] {
+  def push[E >: Nothing](e: E): Stack[E] = new NonEmptyStack[E](e, this)
+  def top: Nothing = throw new IllegalArgumentException("empty stack")
+  def pop: Nothing = throw new IllegalArgumentException("empty stack")
+  def isEmpty: Boolean = true
+}
+
+object Stack  extends App{
+  def apply(): Stack[Nothing] = EmptyStack
+  val intStack: Stack[Int] = Stack()
+  println(intStack)
+}
+
