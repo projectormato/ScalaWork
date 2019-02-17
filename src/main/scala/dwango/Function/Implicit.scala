0ec9c081fb1d1c5f38befc6e0c4801469295aa87
projectormato <k.keita811.anuy@gmail.com>
2019-02-17 21:21:53 +0900

    add Implicit of dwango text.

--- src/main/scala/dwango/Function/Implicit.scala
+++ src/main/scala/dwango/Function/Implicit.scala
@@ -1,5 +1,15 @@
 package dwango.Function
 
-class Implicit {
+object Implicit extends App{
+  implicit class RichString(val src: String) {
+    def smile: String = src + ":-)"
+  }
+  println("Hi ".smile)
+
+  //pimp my libraryパターンで、既存のクラスの利用を便利にするようなimplicit conversionを1つ定義してみてください。
+  implicit class YenInt(val price: Int) {
+    def cost: String = "料金は " + price + " 円です"
+  }
+  println(100.cost)
 
 }
